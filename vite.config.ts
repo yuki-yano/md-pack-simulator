ea28aebe21e356a3918675a2c67cd601030f74bc
yuki_yano <yuki.ycino@gmail.com>
2025-12-01 20:24:33 +0900

    chore: カスタムドメイン対応
    - CNAMEファイルを追加
    - baseパスを/に変更

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/md-pack-simulator/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
